045a9bd07d0ede80b6884dcc1475ef150de3d3ad
Justin Fong <justinvf2018@gmail.com>
2026-04-16 00:00:00 -0700

    enemies spawning on runtime prototype
    - manually place the spawn location object, tag is as melee or ranged
    
    - create object with GroundEnemySpawner.cs and drag in enemy Prefabs

---
+++ Assets/Scripts/GroundEnemySpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GroundEnemySpawner : MonoBehaviour
+{
+    private InputAction spawnEnemy;
+    public GameObject MeleeEnemyPrefab;
+    public GameObject RangedEnemyPrefab;
+
+    public List<GameObject> MeleeSpawnLocations;
+    public List<GameObject> RangedSpawnLocations;
+
+    private void Start()
+    {
+        MeleeSpawnLocations = new List<GameObject>();
+        RangedSpawnLocations = new List<GameObject>();
+        spawnEnemy = InputSystem.actions.FindAction("SpawnEnemy");
+
+    }
+    private void Update()
+    {
+        if(spawnEnemy.WasReleasedThisFrame())
+        {
+            SpawnEnemy(MeleeSpawnLocations, "MeleeSpawner", MeleeEnemyPrefab);
+            SpawnEnemy(RangedSpawnLocations, "RangedSpawner", RangedEnemyPrefab);
+        }
+    }
+
+    private void SpawnEnemy(List<GameObject> spawnLocations, string enemyTypeTagName, GameObject enemyPrefab)
+    {
+        GameObject.FindGameObjectsWithTag(enemyTypeTagName, spawnLocations);
+
+        foreach (GameObject spawn in spawnLocations)
+        {
+            GameObject newEnemy = Instantiate(enemyPrefab, spawn.transform);
+
+            if (newEnemy.TryGetComponent(out EnemyAI enemyAI))
+            {
+                enemyAI.homePoint = spawn.transform;
+            }
+        }
+    }
+}
