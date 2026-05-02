57b958b4a6d19687df97440957afc3f8bcfdf377
Justin Fong <justinvf2018@gmail.com>
2026-05-02 00:01:46 -0700

    update cheat keys
    - PCG level properly in build profile
    - can now access PCG additionally using the cheat keys
    
    - tested 9 slicing with UI elements

--- Assets/Scripts/ComboSceneSkipScripts/CheatKeySceneLoader.cs
+++ Assets/Scripts/ComboSceneSkipScripts/CheatKeySceneLoader.cs
@@ -6,43 +6,49 @@ public class CheatKeySceneLoader : MonoBehaviour
 
 
 
-   void Update()
-   {
-      // Example: Press Control + Shift + B to load "Boss Scene by Alfredo"
-      if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.B))
-      {
-         SceneManager.LoadScene("Boss Scene");
-      }
-      // Example: Press Control + Shift + M to load "Boss Scene by Alfredo"
+    void Update()
+    {
+        // Example: Press Control + Shift + B to load "Boss Scene by Alfredo"
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.B))
+        {
+            SceneManager.LoadScene("Boss Scene");
+        }
+        // Example: Press Control + Shift + M to load "Boss Scene by Alfredo"
 
-      if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.M))
-      {
-         SceneManager.LoadScene("Menus");
-      }
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.M))
+        {
+            SceneManager.LoadScene("Menus");
+        }
 
-      // load tutorial ground
-      if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.Alpha1))
-      {
-         SceneManager.LoadScene("Tutorial Ground");
-      }
+        // load tutorial ground
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("Tutorial Ground");
+        }
 
-      // load tutorial space
-      if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.Alpha2))
-      {
-         SceneManager.LoadScene("Space Level 1");
-      }
+        // load tutorial space
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("Space Level 1");
+        }
 
-      // load planet level 1
-      if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.Alpha3))
-      {
-         SceneManager.LoadScene("Planet 1");
-      }
+        // load planet level 1
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("Planet 1");
+        }
 
-      // load level selector
-      if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.L))
-      {
-         SceneManager.LoadScene("Level Selector 2");
-      }
+        // load planet level 1
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene("PCG_Sample");
+        }
+
+        // load level selector
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Y) && Input.GetKeyDown(KeyCode.L))
+        {
+            SceneManager.LoadScene("Level Selector 2");
+        }
     }
 
 }
