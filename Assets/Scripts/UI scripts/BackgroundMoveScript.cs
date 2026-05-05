03e4ffdfe26cd87f6531596fa3c89ebe1309bda9
PastaChicken <64043214+PastaChicken@users.noreply.github.com>
2026-05-05 12:06:20 -0700

    Main Menu UI revamp
    This push holds all changes done to the UI of only the Main Menu so far

---
+++ Assets/Scripts/UI scripts/BackgroundMoveScript.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundMoveScript : MonoBehaviour
+{
+   [SerializeField] private RawImage backGround;
+   //how much to move it
+   public float moveValue = .1f;
+   public float time = .5f;
+   private float platoe = 1.5f;
+   private Rect moveBackground;
+
+   private void Start()
+   {
+      InvokeRepeating("MoveBackground", 0, time);
+   }
+
+
+   // call every .5 seconds or whatever time is set to
+  
+   void MoveBackground()
+   {
+      moveBackground = new Rect(backGround.uvRect.x + moveValue, backGround.uvRect.y, backGround.uvRect.width, backGround.uvRect.height);
+      backGround.uvRect = moveBackground;
+   }
+}
