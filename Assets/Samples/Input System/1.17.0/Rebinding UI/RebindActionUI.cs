6b32ee3e237dca79a9cca09420bff6b2b1182b5e
Justin Fong <justinvf2018@gmail.com>
2026-06-10 14:20:56 -0700

    add Escape to keybind translation

--- Assets/Samples/Input System/1.17.0/Rebinding UI/RebindActionUI.cs
+++ Assets/Samples/Input System/1.17.0/Rebinding UI/RebindActionUI.cs
@@ -572,6 +572,7 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
             { "↓", "아래쪽 화살표" },
             { "←", "왼쪽 화살표" },
             { "→", "오른쪽 화살표" },
+            { "Esc", "Esc 키" },
         };
 
         private static Dictionary<string, string> englishToChinese = new Dictionary<string, string>()
@@ -593,6 +594,7 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
             { "↓", "下箭头" },
             { "←", "左箭头" },
             { "→", "右箭头" },
+            { "Esc", "Esc键" },
         };
 
         private string TranslateKeyName(string englishKeyName)
